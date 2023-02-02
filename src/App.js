36d94999d363ca8bfb6603512754376acb5a00f1
hanifahprmdta <111113175+hanifahprmdta@users.noreply.github.com>
2023-02-02 15:33:01 +0700

    Revert "removing unnecessary spaces"

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
-import { useContext } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 
-import { UserContext } from "./contexts/user.context";
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import Authentication from './routes/authentication/authentication.component';
 
-import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation/navigation.component";
-import Authentication from "./routes/authentication/authentication.component";
 
 const Shop = () => {
-  return <h1>I am the shop page</h1>;
+  return <h1>I am the Shop page</h1>;
 };
 
 const App = () => {
-  const { currentUser } = useContext(UserContext);
-  return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop" element={<Shop />} />
-        <Route
-          path="auth"
-          element={
-            currentUser ? <Navigate to="/" replace /> : <Authentication />
-          }
-        />
-      </Route>
-    </Routes>
+  return( 
+  <Routes>
+  <Route  path='/navigation' element={<Navigation />} />
+  <Route  index element={<Home />} />
+  <Route  path='shop' element={<Shop />} />
+  <Route  path='auth' element={<Authentication />} />
+  </Routes>
   );
 };
+
 
 export default App;
